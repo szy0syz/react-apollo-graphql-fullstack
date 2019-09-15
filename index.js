08f361938923307cee04d0b5ad6158172b0f5e95
jerry <szy0syz@gmail.com>
2019-09-15 09:38:26 +0800

    Fix mognoose api

--- index.js
+++ index.js
@@ -12,56 +12,55 @@ const server = new ApolloServer({
   resolvers,
 });
 
-mongoose
-  .connect('mongodb://localhost:27017/apollo_serv', { useNewUrlParser: true })
-  .then(async () => {
-    console.log('mongoose connected');
-    let users = await User.find({});
+mongoose.connect('mongodb://localhost:27017/apollo_serv', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', async () => {
+  console.info('[Info] MongoDB is running');
+  let users = await User.find({});
 
-    // init database
-    if (users.length === 0) {
-      console.log('[init database]');
-      users = await User.insertMany([
+  // init database
+  if (users.length === 0) {
+    console.log('[init database]');
+    users = await User.insertMany([
+      {
+        username: 'jerry',
+        password: '123123',
+        email: 'jerry@123.com',
+      },
+      {
+        username: 'szy0syz',
+        password: '123123',
+        email: 'szy0syz@123.com',
+      },
+      {
+        username: 'jerry1',
+        password: '123123',
+        email: 'jerry1@123.com',
+      },
+    ]);
+
+    let posts = await Post.find({});
+    if (posts.length === 0) {
+      Post.insertMany([
         {
           username: 'jerry',
-          password: '123123',
-          email: 'jerry@123.com',
+          body: 'hahahaha',
+          user: users[0]._id,
+        },
+        {
+          username: 'jerry',
+          body: 'hi~~~~',
+          user: users[0]._id,
         },
         {
           username: 'szy0syz',
-          password: '123123',
-          email: 'szy0syz@123.com',
-        },
-        {
-          username: 'jerry1',
-          password: '123123',
-          email: 'jerry1@123.com',
+          body: 'wahaha',
+          user: users[1]._id,
         },
       ]);
-
-      let posts = await Post.find({});
-      if (posts.length === 0) {
-        Post.insertMany([
-          {
-            username: 'jerry',
-            body: 'hahahaha',
-            user: users[0]._id,
-          },
-          {
-            username: 'jerry',
-            body: 'hi~~~~',
-            user: users[0]._id,
-          },
-          {
-            username: 'szy0syz',
-            body: 'wahaha',
-            user: users[1]._id,
-          },
-        ]);
-      }
     }
-    return server.listen({ port: 5100 });
-  })
-  .then(res => {
-    console.log(`Server running at ${res.url}`);
-  });
+  }
+});
+
+server.listen({ port: 5100 }).then(res => {
+  console.info(`[Info] ApolloServer running at ${res.url}`);
+});
