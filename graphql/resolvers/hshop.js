e211d1bbfb05bf04cdd56278063021fa27bf543b
Jerry <szy0syz@gmail.com>
2019-09-27 12:51:12 +0800

    Update

--- graphql/resolvers/hshop.js
+++ graphql/resolvers/hshop.js
@@ -13,10 +13,17 @@ module.exports = {
         throw new Error(error);
       }
     },
-    async getHShops(_, { floor, sortBy, commercialTypeID }) {
+    async getHShops(
+      _,
+      { floor = 1, sortBy, commercialTypeID = 100, pageIndex = 1, pageSize = 10 }
+    ) {
       try {
-        const data = await Shop.find({ floor, commercialTypeID });
-
+        let query = Shop.find({ floor, commercialTypeID })
+          .skip((pageIndex - 1) * pageSize)
+          .limit(pageSize);
+        if (sortBy === 'promotion') query = query.sort({ promotionInfo: -1 });
+        if (sortBy === 'name') query = query.sort({ name: -1 });
+        const data = await query.exec();
         return data;
       } catch (error) {
         throw new Error(error);
